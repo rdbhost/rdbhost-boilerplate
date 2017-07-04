a31d83b94d5f47d12b7b21d8798b0ca2554aee84
David Keeney <dkeeney@rdbhost.com>
2017-07-04 17:13:13 -0600

    Update rdbhost-bootstrap.js

--- js2plain/js/rdbhost-bootstrap.js
+++ js2plain/js/rdbhost-bootstrap.js
@@ -1,31 +1,52 @@
-var $L = $LAB
-    .script('https://www.rdbhost.com/vendor/rdbhost/2.2/lib/js/util-bundle;rdbhost.js').wait(
-        function() {
+(function(undefined) {
+    
+    function hasPromises() {
+        var e = window;
+        return "Promise" in e &&
+            "resolve" in e.Promise && "reject" in e.Promise &&
+            "all" in e.Promise && "race" in e.Promise &&
+            function () {
+                var n;
+                new e.Promise(function (e) {
+                    n = e
+                });
+                return "function" == typeof n;
+            }()
+    }
+    function hasFetch() {
+        return "fetch" in window;
+    }
 
-            if ( !Rdbhost.featuredetects.hasPromises() )
-                $L = $L.script('https://www.rdbhost.com/vendor/es6-promises/dist/es6-promise.js');
-            if ( !Rdbhost.featuredetects.hasFetch() )
-                $L = $L.script('https://www.rdbhost.com/vendor/fetch/fetch.js').wait();
-            $L = $L.script('https://www.rdbhost.com/vendor/rdbhost/2.2/lib/js/rdb2-livereload.js');
+    var $L = $LAB;
+    
+    if ( !hasPromises() )
+          $L = $L.script('https://www.rdbhost.com/vendor/es6-promises/dist/es6-promise.js').wait();
+    if ( !hasFetch() )
+          $L = $L.script('https://www.rdbhost.com/vendor/fetch/fetch.js').wait();
 
-            // -- uncomment these if you need them
-            // $L = $L.script('https://www.rdbhost.com/vendor/rdbhost/2.2/lib/js/rdb2-emailing.js');
-            // $L = $L.script('https://www.rdbhost.com/vendor/rdbhost/2.2/lib/js/rdb2-authenticate.js');
-            // $L = $L.script('https://www.rdbhost.com/vendor/rdbhost/2.2/lib/js/rdb2-charge.js');
+    if ( !window.Rdbhost || !window.Rdbhost.preauth ) {     
+        $L = $L.script('https://www.rdbhost.com/vendor/rdbhost/2.2/lib/js/util-bundle;rdbhost.js').wait();
 
+    // -- uncomment these if you need them
+    // $L = $L.script('https://www.rdbhost.com/vendor/rdbhost/2.2/lib/js/rdb2-emailing.js');
+    // $L = $L.script('https://www.rdbhost.com/vendor/rdbhost/2.2/lib/js/rdb2-authenticate.js');
+    // $L = $L.script('https://www.rdbhost.com/vendor/rdbhost/2.2/lib/js/rdb2-charge.js');
 
-            // $L = $L.script('https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js');
-            $L = $L.script('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js');
-            $L = $L.script('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js');
-            // $L = $L.script('');
+    // $L = $L.script('https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.js');
+    // $L = $L.script('https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js');
+    // $L = $L.script('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.min.js');
+    // $L = $L.script('https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js');
+    $L = $L.script('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js');
+    $L = $L.script('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js');
+    // $L = $L.script('');
 
-            $L = $L.script('js/example.js').wait(function() {
+    $L = $L.script('https://www.rdbhost.com/vendor/rdbhost/2.2/lib/js/rdb2-livereload.js').wait();    
 
-                Rdbhost.connect('www.rdbhost.com', 1866);
-                var preauth_for_reloader = Rdbhost.preauth();
-                Rdbhost.activate_reloader(preauth_for_reloader);
+    $L = $L.script('js/app.js').wait(function() {
 
-                run();
-
-            });
-        });
+        Rdbhost.connect(/* HOST_NAME */'www.rdbhost.com', /* ACCOUNT_NUMBER */1866);
+        var preauth_for_reloader = Rdbhost.preauth();
+        Rdbhost.activate_reloader(preauth_for_reloader);
+        run();
+    });
+})();
