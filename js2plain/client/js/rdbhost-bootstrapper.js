fbf84b3dc32c3b460a8c75d7a987eacab5c206e9
David <dkeeney@rdbhost.com>
2017-11-08 02:10:38 +0000

    misc

--- js2plain/client/js/rdbhost-bootstrapper.js
+++ js2plain/client/js/rdbhost-bootstrapper.js
@@ -30,7 +30,7 @@
 		Rdbhost.use_labjs_loader($LAB);
 	}
 	if ( !window.Rdbhost || !window.Rdbhost.preauth ) {
-		$L = $L.script('https://www.rdbhost.com/vendor/rdbhost/2.2/lib/js/util-bundle;rdbhost.js')
+		$L = $L.script('https://www.rdbhost.com/vendor/rdbhost/2.3/lib/js/util-bundle;rdbhost.js')
 			.wait(_init);
 	} else {
 		_init();
@@ -39,11 +39,11 @@
 	// -- uncomment these if you need them
 	//
 	// if (!Rdbhost.Email)
-	//   $L = $L.script('https://www.rdbhost.com/vendor/rdbhost/2.2/lib/js/rdb2-emailing.js');
+	//   $L = $L.script('https://www.rdbhost.com/vendor/rdbhost/2.3/lib/js/rdb2-emailing.js');
 	// if (!Rdbhost.Authenticate)
-	//   $L = $L.script('https://www.rdbhost.com/vendor/rdbhost/2.2/lib/js/rdb2-authenticate.js');
+	//   $L = $L.script('https://www.rdbhost.com/vendor/rdbhost/2.3/lib/js/rdb2-authenticate.js');
 	// if (!Rdbhost.Charge)
-	//   $L = $L.script('https://www.rdbhost.com/vendor/rdbhost/2.2/lib/js/rdb2-charge.js');
+	//   $L = $L.script('https://www.rdbhost.com/vendor/rdbhost/2.3/lib/js/rdb2-charge.js');
 
 	// $L = $L.script('https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.js');
 	// $L = $L.script('https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js');
@@ -51,11 +51,9 @@
 	// $L = $L.script('https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js');
 	$L = $L.script('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js');
 	$L = $L.script('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js');
-	// $L = $L.script('https://cdnjs.cloudflare.com/ajax/libs/crossroads/0.12.2/crossroads.min.js');
-	// $L = $L.script('https://cdn.rawgit.com/visionmedia/page.js/master/page.js');
 	// $L = $L.script('');
 
-	$L = $L.script('https://www.rdbhost.com/vendor/rdbhost/2.2/lib/js/rdb2-livereload.js')
+	$L = $L.script('https://www.rdbhost.com/vendor/rdbhost/2.3/lib/js/rdb2-livereload.js')
 		.wait(function() {
 			var preauth_for_reloader = Rdbhost.preauth();
 			Rdbhost.activate_reloader(preauth_for_reloader);
