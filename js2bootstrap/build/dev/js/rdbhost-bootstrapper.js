88ecd3de8a906a35a3af83fa8abdec6d13a7c537
David <dkeeney@travelbyroad.net>
2017-11-13 08:38:37 -0700

    used 'production' rawgit link for page.js

--- js2bootstrap/build/dev/js/rdbhost-bootstrapper.js
+++ js2bootstrap/build/dev/js/rdbhost-bootstrapper.js
@@ -52,7 +52,7 @@
 	$L = $L.script('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js');
 	$L = $L.script('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js');
 	// $L = $L.script('https://cdnjs.cloudflare.com/ajax/libs/crossroads/0.12.2/crossroads.min.js');
-	// $L = $L.script('https://cdn.rawgit.com/visionmedia/page.js/master/page.js');
+	// $L = $L.script('https://cdn.rawgit.com/visionmedia/page.js/1.7.1/page.js');
 	// $L = $L.script('');
 
 	$L = $L.script('https://www.rdbhost.com/vendor/rdbhost/2.3/lib/js/rdb2-livereload.js')
